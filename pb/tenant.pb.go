d8fc367fcb0efdea0615e086c9c2fc3b00753419
DanielChachagua <danielmchachagua@gmail.com>
2026-01-17 17:49:11 -0300

    update tenant model

--- pb/tenant.pb.go
+++ pb/tenant.pb.go
@@ -192,7 +192,7 @@ type TenantResponse struct {
 	Email         string                 `protobuf:"bytes,6,opt,name=email,proto3" json:"email,omitempty"`
 	SettingTenant *SettingTenant         `protobuf:"bytes,7,opt,name=setting_tenant,json=settingTenant,proto3" json:"setting_tenant,omitempty"`
 	TokenMp       *string                `protobuf:"bytes,8,opt,name=token_mp,json=tokenMp,proto3,oneof" json:"token_mp,omitempty"`
-	TokenEmial    *string                `protobuf:"bytes,9,opt,name=token_emial,json=tokenEmial,proto3,oneof" json:"token_emial,omitempty"`
+	TokenEmail    *string                `protobuf:"bytes,9,opt,name=token_email,json=tokenEmail,proto3,oneof" json:"token_email,omitempty"`
 	unknownFields protoimpl.UnknownFields
 	sizeCache     protoimpl.SizeCache
 }
@@ -283,9 +283,9 @@ func (x *TenantResponse) GetTokenMp() string {
 	return ""
 }
 
-func (x *TenantResponse) GetTokenEmial() string {
-	if x != nil && x.TokenEmial != nil {
-		return *x.TokenEmial
+func (x *TenantResponse) GetTokenEmail() string {
+	if x != nil && x.TokenEmail != nil {
+		return *x.TokenEmail
 	}
 	return ""
 }
@@ -604,10 +604,10 @@ const file_tenant_proto_rawDesc = "" +
 	"\x05email\x18\x06 \x01(\tR\x05email\x12<\n" +
 	"\x0esetting_tenant\x18\a \x01(\v2\x15.tenant.SettingTenantR\rsettingTenant\x12\x1e\n" +
 	"\btoken_mp\x18\b \x01(\tH\x00R\atokenMp\x88\x01\x01\x12$\n" +
-	"\vtoken_emial\x18\t \x01(\tH\x01R\n" +
-	"tokenEmial\x88\x01\x01B\v\n" +
+	"\vtoken_email\x18\t \x01(\tH\x01R\n" +
+	"tokenEmail\x88\x01\x01B\v\n" +
 	"\t_token_mpB\x0e\n" +
-	"\f_token_emial\"/\n" +
+	"\f_token_email\"/\n" +
 	"\rTenantRequest\x12\x1e\n" +
 	"\n" +
 	"identifier\x18\x01 \x01(\tR\n" +
